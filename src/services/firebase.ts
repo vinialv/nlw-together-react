c3b04cdd2274128631123a871479fab35ae7c92a
Vinícius Oliveira Alves <vini.alv15@hotmail.com>
2021-06-20 22:57:46 -0300

    Initial commit

---
+++ src/services/firebase.ts
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+
+import 'firebase/auth';
+import 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGIN_SENDER_ID,
+    appId: process.env.APP_ID
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const database = firebase.database();
